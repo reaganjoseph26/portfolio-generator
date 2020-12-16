dfaa1c701136f4f3b43ea030d5d809a5862eeb07
Racer <reaganjoseph26@yahoo.com>
2020-12-16 18:32:54 -0500

    sections added to html

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-// const pageHTML = generatePage(name, github)
-
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw new Error(err);
-  
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-//   });
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer
@@ -142,7 +134,13 @@ if (!portfolioData.projects) {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData)
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+      
+        // console.log('Portfolio complete! Check out index.html to see the output!');
+      });
+
   });
   
 
